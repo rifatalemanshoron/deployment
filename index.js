af86a030ddbcc67e2b94e0390970e2fbab686e0e
rifatalemanshoron <rifatalemanshoron@gmail.com>
2024-09-04 15:50:56 +0600

    Port is Printed now

--- index.js
+++ index.js
@@ -3,9 +3,9 @@ const app = express();
 const PORT = process.env.PORT || 8000;
 
 app.get("/", (req, res)=>{
-    return res.send("hello");
+    return res.send("hello, I have Change code");
 })
 
 app.listen(PORT, ()=>{
-    console.log("SERVER STARTED SUCCESSFULLY");
+    console.log("SERVER STARTED SUCCESSFULLY AT PORT ", PORT);
 })
